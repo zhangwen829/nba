2f431170536a81a6b83a97406c258496d9764185
Wen Zhang <zhangwenfd@gmail.com>
2018-10-18 19:45:59 -0400

    pass data down, style display

--- src/components/Main.js
+++ src/components/Main.js
@@ -26,7 +26,7 @@ export default class Main extends React.Component {
   render() {
     return (
       <div className="main">
-        <Profile />
+        <Profile playerInfo={this.state.playerInfo} />
         <div>ShotChart</div>
       </div>
     );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 export default class Main extends React.Component {
+
   render() {
+    console.log(this.props.playerInfo);
+    const {
+      playerId,
+      playerName,
+      teamCity,
+      teamName,
+      teamAbbreviation,
+      height,
+      weight,
+      pts,
+      reb,
+      ast,
+      pie,
+    } = this.props.playerInfo;
+
     return (
-      <div className="profile">Profile</div>
+      <div className="profile">
+        <div className="profile-entry player-name">{`${playerName}`}</div>
+        <div className="profile-entry">
+          <div className="profile-entry-left">Team</div>
+          <div className="profile-entry-right">{`${teamCity} ${teamName}`}</div>
+        </div>
+      </div>
     );
   }
 } 
