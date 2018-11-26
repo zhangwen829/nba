cb2b51dcd87ed957133de2c3bc324705021cd88d
Wen Zhang <zhangwenfd@gmail.com>
2018-11-25 22:24:03 -0500

    search bar functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { AutoComplete, Input, Icon } from 'antd';
+import nba from 'nba';
+import { PROFILE_PIC_URL_PREFIX } from '../constants';
+
+
+const Option = AutoComplete.Option;
 
 function onSelect(value) {
   console.log('onSelect', value);
@@ -12,11 +17,16 @@ export default class SearchBar extends React.Component {
 
   handleSearch = (value) => {
     this.setState({
-      dataSource: !value ? [] : [
-        value,
-        value + value,
-        value + value + value,
-      ],
+      dataSource: !value ? [] : nba.searchPlayers(value).map(({ fullName, playerId }) =>
+        <Option key={playerId}>
+          <img
+            className="player-option-pic"
+            src={`${PROFILE_PIC_URL_PREFIX}/${playerId}.png`}
+            alt="Profile"
+          />
+          <span className="player-option-label">{fullName}</span>
+        </Option>
+      )
     });
   }
 
