25e3e0343d1404485aa5f006c0ea95fd313d4b39
Wen Zhang <zhangwenfd@gmail.com>
2018-11-25 22:53:23 -0500

    clean up

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,13 @@ import nba from 'nba';
 import Profile from './Profile'
 import DataViewContainer from './DataViewContainer';
 import SearchBar from './SearchBar';
+import { DEFAULT_PLAYER_INFO } from '../constants';
 
 window.nba = nba;
 
 export default class Main extends React.Component {
   state = {
-    playerInfo: {
-      playerId: 201939,
-      playerName: 'Stephen Curry'
-    }
+    playerInfo: DEFAULT_PLAYER_INFO
   }
 
   componentDidMount() {
